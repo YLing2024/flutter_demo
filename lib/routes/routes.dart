fd12af23ad829a6cbf40f4881151e6180cf7fbf8
zhangyunling <1461428394@qq.com>
2025-10-28 13:59:41 +0800

    feat(路由): 添加路由配置并清理多余注释
    - 新增routes.dart文件定义应用路由
    - 在main.dart中引入路由配置并设置初始路由
    - 清理main.dart中的冗余注释
    - 添加VS Code调试配置文件launch.json

---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_demo/main.dart';
+
+Map<String, WidgetBuilder> routes = {
+  //首页
+  "/": (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+};
